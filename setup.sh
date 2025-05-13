c658a82ad298ddf7db5d66578adc6731c1c76b9e
Alex Lokhman <alex.lokhman@brandwidth.com>
2025-05-13 11:03:44 +0100

    update with bw changes

--- setup.sh
+++ setup.sh
@@ -324,13 +324,13 @@ ipsec restart
 # echo "${LOGINUSERNAME}:${LOGINPASSWORD}" | chpasswd
 # adduser "${LOGINUSERNAME}" sudo
 
-sed -r \
--e "s/^#?Port 22$/Port ${SSHPORT}/" \
--e 's/^#?LoginGraceTime (120|2m)$/LoginGraceTime 30/' \
--e 's/^#?PermitRootLogin yes$/PermitRootLogin no/' \
--e 's/^#?X11Forwarding yes$/X11Forwarding no/' \
--e 's/^#?UsePAM yes$/UsePAM no/' \
--i.original /etc/ssh/sshd_config
+# sed -r \
+# -e "s/^#?Port 22$/Port ${SSHPORT}/" \
+# -e 's/^#?LoginGraceTime (120|2m)$/LoginGraceTime 30/' \
+# -e 's/^#?PermitRootLogin yes$/PermitRootLogin no/' \
+# -e 's/^#?X11Forwarding yes$/X11Forwarding no/' \
+# -e 's/^#?UsePAM yes$/UsePAM no/' \
+# -i.original /etc/ssh/sshd_config
 
 # if [[ $CERTLOGIN = "y" ]]; then
 #   mkdir -p "/home/${LOGINUSERNAME}/.ssh"
